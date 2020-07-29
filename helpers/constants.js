9ae059feddea9661c6bdedb65748ff187f074786
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-07-29 11:00:58 -0500

    Change text email

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,7 +1,7 @@
 const EMAIL = {
   WELCOME_SUBJECT: '¡Bienvenid@ a Capazy!',
   WELCOME_BODY:
-    'Te damos la bienvenida a la comunidad de Capazy. Para nosotros es un placer que estés aquí. Para continuar con el proceso y que la búsqueda de un trabajo remoto sea exitosa necesitamos validar tu nivel de inglés. Si ya cuentas con un certificado vigente puedes subirlo, sino puedes realizar este test: https://www.efset.org/quick-check/ y descargar tu resultado. Luego entra a https://capazy.com, ve a tu perfil y adjunta tu certificado en el botón "Upload your English Certificate". Una vez lo subas, un reclutador te contactará para los siguientes pasos.',
+    'Te damos la bienvenida a la comunidad de Capazy. Para nosotros es un placer que estés aquí. Para continuar con el proceso y que la búsqueda de un trabajo remoto sea exitosa necesitamos validar tu nivel de inglés. Si ya cuentas con un certificado vigente puedes subirlo, sino puedes realizar este test gratuito: https://www.efset.org/quick-check/ y descargar tu resultado. Luego entra a https://capazy.com, ve a tu perfil y adjunta tu certificado en el botón "Upload your English Certificate". Una vez lo subas, un reclutador te contactará para los siguientes pasos.',
 };
 
 module.exports = { EMAIL };
